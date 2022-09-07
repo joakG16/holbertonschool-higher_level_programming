2089d16638c41fadd9f7e56f7859e2456c6811bb
joakG16 <4379@holbertonschool.com>
2022-09-07 15:23:44 -0300

    task 1 zero case trying to fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 
 if last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
-if last_dig == 0:
+elif last_dig == 0:
     print(f"Last digit of {number} is {last_dig} and is 0")
-if last_dig < 6 and not 0:
+elif last_dig < 6 and last_dig != 0:
     print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
