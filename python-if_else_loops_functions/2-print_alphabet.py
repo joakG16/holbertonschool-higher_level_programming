5fdc7dc758bb4994e529cf4d236551f46db5cf2d
joakG16 <4379@holbertonschool.com>
2022-09-07 20:13:19 -0300

    task 2, end had single quotes

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for a in range (97, 123):
-    print(f"{chr(a)}", end='') # Any text outside of curly braces '{}' is printed out directly.
+    print(f"{chr(a)}", end="") # Any text outside of curly braces '{}' is printed out directly.
