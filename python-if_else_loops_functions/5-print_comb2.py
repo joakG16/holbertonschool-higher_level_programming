ed74b1efd07afb7b4360a80fee5ac7fcff2e1ac5
joakG16 <4379@holbertonschool.com>
2022-09-07 22:30:43 -0300

    task 2 comments

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(0, 99):
-    print("{}, ".format(a), end="")
-print("99")
+    print("{:02}, ".format(a), end="")
+print("99") #{:02} will padd a zero if it is a one-digit number
