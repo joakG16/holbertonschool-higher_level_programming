1f7ef33d5a491663d9b33e2ed33cf1b18e24592d
joakG16 <4379@holbertonschool.com>
2022-09-08 07:32:56 -0700

    task 6 pep8

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range (0, 9):
+for i in range(0, 9):
     if i == 8:
-        print (89)
+        print(89)
     else:
-        for j in range (i + 1, 10):
+        for j in range(i + 1, 10):
             print("{}{}, ".format(i, j), end="")
