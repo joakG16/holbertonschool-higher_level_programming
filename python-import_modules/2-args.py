f51b434b6e10e02bfc4a346cbf9f7555465a6ff5
joakG16 <4379@holbertonschool.com>
2022-09-09 16:45:28 -0300

    colon added to strings

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,12 +3,12 @@ if __name__ == '__main__':
     import sys
 
     len_a = len(sys.argv)
-    op1 = "argument"
-    op2 = "arguments"
+    op1 = "argument:"
+    op2 = "arguments:"
 
     if len_a < 2:
         print("0 arguments.")
     else:
-        print("{0}: {1}".format(len_a - 1, op1 if (len_a == 2) else op2))
+        print("{0} {1}".format(len_a - 1, op1 if (len_a == 2) else op2))
         for i in range(1, len_a):
             print("{0}: {1}".format(i, sys.argv[i]))
