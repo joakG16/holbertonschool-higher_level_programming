89cfe63e39f34b8c6694d43c6080b08904a41b26
joakG16 <4379@holbertonschool.com>
2022-09-13 11:20:34 -0700

    wrong style

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0
+    if idx < 0:
         return my_list
     for i in range(0, len(my_list)):
         if i == idx:
