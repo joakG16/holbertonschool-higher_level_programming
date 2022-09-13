4b93ffefb1e9f4230370a14cde7823e0482fa27d
joakG16 <4379@holbertonschool.com>
2022-09-13 11:44:45 -0700

    task 4, case in which index is same as lenght of list was not being taken care of

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,9 +3,7 @@
 
 def new_in_list(my_list, idx, element):
     list_copy = my_list.copy()
-    if idx < 0:
-        return list_copy
-    elif idx > len(list_copy):
+    if idx < 0 or idx >= len(my_list):
         return list_copy
     else:
         list_copy[idx] = element
