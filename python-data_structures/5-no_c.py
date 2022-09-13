c32c80718c2b4384ddf2d3290e97799838f27f72
joakG16 <4379@holbertonschool.com>
2022-09-13 12:36:41 -0700

    task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    no_c_str = '' ''' defining a new string, since they are inmutable '''
+    for ch in my_string:
+        ''' append the ch according to the condition '''
+        if ch == 'c' or ch == 'C':
+            no_c_str += ''
+        else:
+            no_c_str += ch
+    return no_c_str
