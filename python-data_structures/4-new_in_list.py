f90f4fa0cbaf99970825e20656a5265c15ca3828
joakG16 <4379@holbertonschool.com>
2022-09-13 11:38:55 -0700

    task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,11 +4,11 @@ from http.client import FOUND
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
-        return my_list.copy()
     list_copy = my_list.copy()
-    for item in range(len(list_copy)):
-        if list_copy[item] == idx:
-            list_copy[item] == element
-            print(item)
-            return list_copy
+    if idx < 0:
+        return list_copy
+    elif idx > len(list_copy):
+        return list_copy
+    else:
+        list_copy[idx] = element
+        return list_copy
