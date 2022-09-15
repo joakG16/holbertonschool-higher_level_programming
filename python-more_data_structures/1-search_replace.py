371574771e1a1abcadeae9f8ce7496d3be50db2f
joakG16 <4379@holbertonschool.com>
2022-09-15 15:58:29 -0300

    task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' this function will populate the list from test with replace if condition
+is true, else will populte it with value'''
+
+
+def search_replace(my_list, search, replace):
+    if my_list:
+        return [replace if value == search else value for value in my_list]
