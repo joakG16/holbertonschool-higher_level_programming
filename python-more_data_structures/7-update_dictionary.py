0500385b12caca3443557c4dbbf48bdf3d1776f3
joakG16 <4379@holbertonschool.com>
2022-09-16 06:33:43 -0300

    task 7 done

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' The update() method will update the dictionary with the items from a given
+argument. If the item does not exist, the item will be added. '''
+
+
+def update_dictionary(a_dictionary, key, value):
+    update_dic = a_dictionary
+    update_dic.update({key:value})
+    return update_dic
