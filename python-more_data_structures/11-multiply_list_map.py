b616cbb8c2d0a591c5f10b0dc3f24e1558a1a6e7
JoakG16 <4379@holbertonstudents.com>
2022-09-16 18:54:54 +0000

    lines from file exceed 3 lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -3,5 +3,4 @@
 
 def multiply_list_map(my_list=[], number=0):
     copy_list = my_list.copy()
-    copy_list = list(map(lambda value: value * number, copy_list))
-    return copy_list
+    return list(map(lambda value: value * number, copy_list))
