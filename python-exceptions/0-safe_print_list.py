7d2579faecf4f4d37960ce857bb332f7beb0c22f
joakG16 <4379@holbertonschool.com>
2022-09-17 15:29:00 -0300

    some coments to understand better task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+''' range(x) goes from 0 to x - 1, remember '''
 
 
 def safe_print_list(my_list=[], x=0):
@@ -6,6 +7,8 @@ def safe_print_list(my_list=[], x=0):
         for element in range(x):
             try:
                 print("{}".format(my_list[element]), end="")
+                ''' the next line it's just for the return value, because element
+                will be restarted to keep with the sequence established on the range'''
                 element += 1
             except IndexError:
                 break
