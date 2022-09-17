cb9e6281b34c2083a83dfb66f1919d72f9705950
joakG16 <4379@holbertonschool.com>
2022-09-17 16:33:33 -0300

    pep9 multiple statements on a single line fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
     while index < x:
         try:
             print("{:d}".format(my_list[index]), end="")
-            index += 1; count += 1
+            index += 1
+            count += 1
         except (ValueError, TypeError):
             ''' index will change, but not count because, as an error
             it won't be printed'''
