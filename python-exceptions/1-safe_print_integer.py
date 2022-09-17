2c875194ee4d6100e0b787dcfa5a771127da2546
joakG16 <4379@holbertonschool.com>
2022-09-17 15:36:49 -0300

    task 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        ''' if print cause of value falls into error, return line won't be executed '''
+        return True
+    except ValueError:
+        return False
