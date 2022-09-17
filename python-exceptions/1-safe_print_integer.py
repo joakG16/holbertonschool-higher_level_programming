35b032c57c01de9714f34d65bb46031bb52f4ca6
joakG16 <4379@holbertonschool.com>
2022-09-17 15:38:23 -0300

    TypeError added as a case

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,5 +6,5 @@ def safe_print_integer(value):
         print("{:d}".format(value))
         ''' if print cause of value falls into error, return line won't be executed '''
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
