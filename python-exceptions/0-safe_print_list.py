79e3930c9b4c881dc10599c4000fea5d0a584373
joakG16 <4379@holbertonschool.com>
2022-09-17 14:17:04 -0300

    identation error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         element = 0
         for element in range(x):
             try:
-                  print("{}".format(my_list[element]), end="")
-                  element += 1
+                print("{}".format(my_list[element]), end="")
+                element += 1
             except IndexError:
                 break
         print()
