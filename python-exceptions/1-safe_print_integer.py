748656f884f45796929d796d2a91f9581bd08bae
joakG16 <4379@holbertonschool.com>
2022-09-17 15:39:21 -0300

    pep8 fixed

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        ''' if print cause of value falls into error, return line won't be executed '''
+        ''' if print cause of value falls into error,
+        return line won't be executed '''
         return True
     except (ValueError, TypeError):
         return False
