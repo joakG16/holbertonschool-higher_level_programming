10403375ede03cc91f4d6a7de25da1ada38f7109
joakG16 <4379@holbertonschool.com>
2022-09-22 18:39:52 -0300

    pep8 validation task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,7 +2,7 @@
 ''' Class Square created, used to define a square with respective,
 size as argument passed, later on to be private to ensure
 type/validation and how it will be retrieved from outside, done
-by using the property attribute'''
+by using the property attribute. '''
 
 
 class Square:
@@ -10,7 +10,7 @@ class Square:
     This method has the line self.size = size.
     This expression automatically calls size().'''
     def __init__(self, size=0):
-        self.size = size # self.propertyobject = argument passed (size)
+        self.size = size  # self.propertyobject = argument passed (size)
 
     ''' Simply put, property attaches some code (getter and setter)
     to the member attribute accesses (size). '''
@@ -19,7 +19,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     ''' size is called even when we create an object. '''
     @size.setter
     def size(self, value):
@@ -29,7 +29,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     ''' The actual size value is stored in the
     private __size variable. The size *attribute* is a property
     object which provides an interface to this private variable. '''
