c9a43563a4652d9b944b04f17398e07d4aef2b08
joakG16 <4379@holbertonschool.com>
2022-09-24 16:59:29 -0300

    used same number for diferent modules

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -13,23 +13,31 @@ class TestMaxInteger(unittest.TestCase):
         with self.assertRaises(TypeError):
             max_integer([4, 'error', 3, 6])
 
-    def test2(self):
+    def test1(self):
         ''' None '''
         self.assertRaises(TypeError, max_integer, None)
 
+    def test2(self):
+        ''' max at the end '''
+        self.assertEqual(max_integer([1, 2, 7]), 7)
+
     def test3(self):
-        ''' Float '''
-        self.assertEqual(max_integer([7.7]), 7.7)
+        ''' max at the beginning '''
+        self.assertEqual(max_integer([7, 3, 2, 4, 1]), 7)
 
     def test4(self):
-        ''' Same number '''
-        self.assertEqual(max_integer([7, 7, 7, 7, 7]), 7)
-
+        ''' max in the middle '''
+        self.assertEqual(max_integer([2, 4, 8, 5, 3]), 8)
+        
     def test5(self):
-        ''' Negatives '''
-        self.assertEqual(max_integer([-7, -9, 2, 37, -44, 52]), 52)
+        ''' one negative number '''
+        self.assertEqual(max_integer([-7, 9, 2, 37, 44, 52]), 52)
     
-    def test5(self):
+    def test6(self):
+        ''' only negatives '''
+        self.assertEqual(max_integer([-7, -9, -2, -37, -44, -52]), -2)
+    
+    def test7(self):
         ''' Empty list '''
         self.assertEqual(max_integer([]), None)
 
