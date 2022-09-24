8c8907577712929bf4df04b13bf2adf88216ac5c
joakG16 <4379@holbertonschool.com>
2022-09-24 16:21:09 -0300

    task 5 remade for "char.char" case

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,9 +9,17 @@ def text_indentation(text):
     ''' check if text passed is type str'''
     if type(text) is not str:
         raise TypeError("text must be a string")
-
-    '''replacing characters'''
-    replaced_text = text.replace(". ", ".\n\n")\
-        .replace("? ", "?\n\n").replace(": ", ":\n\n")
-    print(replaced_text)
+    
+    after_new_line = False
+    for char in text:
+        if after_new_line:
+            if char == " ": # if there was a space after the new line printed
+                continue
+            after_new_line = False
+        if char == '.' or char == '?' or char == ':':
+            print(char)
+            print()
+            after_new_line = True # after symbol found, there could be a space
+        else:
+            print(char, end="") # keep printing other symbols
         
