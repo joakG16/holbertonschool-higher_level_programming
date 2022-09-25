6b9f28184b9a308d4051d4092ab3d8462b43e02f
joakG16 <4379@holbertonschool.com>
2022-09-25 11:18:14 -0300

    pep8 task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,17 +9,16 @@ def text_indentation(text):
     ''' check if text passed is type str'''
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
+
     after_new_line = False
     for char in text:
         if after_new_line:
-            if char == " ": # if there was a space after the new line printed
+            if char == " ":  # if there was a space after the new line printed
                 continue
             after_new_line = False
         if char == '.' or char == '?' or char == ':':
             print(char)
             print()
-            after_new_line = True # after symbol found, there could be a space
+            after_new_line = True  # after sym. found, there could be a space
         else:
-            print(char, end="") # keep printing other symbols
-        
+            print(char, end="")  # keep printing other symbols
