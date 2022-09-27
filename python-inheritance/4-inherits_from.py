08546f7026a6e19bfbed6a9bafa029d972183893
joakG16 <4379@holbertonschool.com>
2022-09-27 09:41:49 -0300

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,7 +12,7 @@ methods are quite similar. However, the name itself explains the differences.
 isinstance() tests whether the object is an instance or subclass of classinfo.
 Whereas, issubclass() only checks if it is a subclass of classinfo or not
 (does not check object relationship). '''
-    if issubclass(obj, a_class) is True:
+    if issubclass(type(obj), a_class) is True:
         return True
     else:
         return False
