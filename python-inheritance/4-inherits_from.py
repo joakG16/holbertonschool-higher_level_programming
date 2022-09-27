6d19480d4ad3d1e6719a37c1232e469f0967a968
joakG16 <4379@holbertonschool.com>
2022-09-27 09:00:06 -0300

    task 4 done

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+    A function that returns True if the object is an instance of a
+class that inherited (directly or indirectly) from the specified
+class ; otherwise False.
+'''
+
+
+def inherits_from(obj, a_class):
+    ''' Don't get confused between isinstance() and issubclass() as both
+methods are quite similar. However, the name itself explains the differences.
+isinstance() tests whether the object is an instance or subclass of classinfo.
+Whereas, issubclass() only checks if it is a subclass of classinfo or not
+(does not check object relationship). '''
+    if issubclass(obj, a_class) is True:
+        return True
+    else:
+        return False
