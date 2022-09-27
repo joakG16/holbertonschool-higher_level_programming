68394650a1903c713863316f0a31818a10700471
joakG16 <4379@holbertonschool.com>
2022-09-27 14:15:20 -0300

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' based on previous task, write a class that raises an exception '''
+
+
+class Basegeometry:
+    ''' defining method '''
+    def area(self):
+        raise Exception("area() is not implemented")
