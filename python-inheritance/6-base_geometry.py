36137b60f1a48e811e0e4a2bed9892d3b236f8f3
joakG16 <4379@holbertonschool.com>
2022-09-27 14:17:54 -0300

    class not documented

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -2,7 +2,7 @@
 ''' based on previous task, write a class that raises an exception '''
 
 
-class Basegeometry:
-    ''' defining method '''
+class BaseGeometry:
+    ''' defining method (attrribute)'''
     def area(self):
         raise Exception("area() is not implemented")
