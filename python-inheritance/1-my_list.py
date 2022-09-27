9deb1e214438d4969c3f54617a6e88111a79d64f
joakG16 <4379@holbertonschool.com>
2022-09-27 06:37:29 -0300

    pep8 validation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 ''' this module creates a Class that inherits from list '''
 
+
 class MyList(list):
     ''' with list as superclass, this new one
     will inherit its attributes '''
 
     def print_sorted(self):
-        ''' The used function will create a new list containing a 
+        ''' The used function will create a new list containing a
         sorted version of the list it is given. The sorted()
         function will not modify the list(self) passed as a parameter'''
         print(sorted(self))
