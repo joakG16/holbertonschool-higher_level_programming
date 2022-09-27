542e4e27473cd2b307e7b50cc48f10d275f9e4b3
joakG16 <4379@holbertonschool.com>
2022-09-27 05:42:10 -0300

    pep8 validation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 ''' a function that returns the list of available
 attributes and methods of an object '''
 
+
 def lookup(obj):
     ''' dir() tries to return a valid list of attributes
     of the object it is called upon. '''
