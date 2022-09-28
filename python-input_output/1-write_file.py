6a845ccd82f77047d164d7dc0d9040a18f8f6a76
JoakG16 <4379@holbertonstudents.com>
2022-09-28 20:17:50 +0000

    task 1 corrected and task 2 done

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-''' A function that writes a string to a text file (UTF8) and
-returns the number of characters written '''
+''' A function that ((if created already ) over)writes a string to a
+text file (UTF8) and returns the number of characters written '''
 
 
 def write_file(filename="", text=""):
-    ''' creating a file object through open, append mode specified '''
-    with open(filename, mode='a', encoding='utf-8') as file:
+    ''' creating a file object through open, write(w) mode specified '''
+    with open(filename, mode='w', encoding='utf-8') as file:
         file.write(text)
         return file.tell()  # The tell() method returns the current
         # file position in a file stream.
---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' a function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added '''
+
+
+def append_write(filename="", text=""):
+    ''' creating a file object through open, append(a) mode specified '''
+    with open(filename, mode='a', encoding='utf-8') as a_file:
+        a_file.write(text)
+        return len(text)
