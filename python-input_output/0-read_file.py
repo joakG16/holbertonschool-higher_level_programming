ba1f2b76029db7a1f57dbf056f46727ea7467bfa
JoakG16 <4379@holbertonstudents.com>
2022-09-28 19:21:21 +0000

    task 0 done

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' A function that reads a text file (UTF8) and prints it to stdout '''
+
+
+def read_file(filename=""):
+    ''' open will return a {file object, "file" in this case}, if mode
+    is ommited, default is {r}, as for read mode'''
+    with open(filename, encoding="utf-8") as file:
+        read_data = file.read()  # method that returns some
+        # quantity of data and returns a string
+        print(read_data)
