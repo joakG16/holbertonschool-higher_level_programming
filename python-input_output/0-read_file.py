1420948885576c4927b75e774fa741ab5e8dd002
JoakG16 <4379@holbertonstudents.com>
2022-09-28 19:23:27 +0000

    extra newline fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,5 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as file:
         read_data = file.read()  # method that returns some
         # quantity of data and returns a string
-        print(read_data)
+        print(read_data, end="")  # end specified since newline from file
+        # is also read
