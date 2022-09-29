826645961faaea471dfa5bbda843a86ccf018131
joakG16 <4379@holbertonschool.com>
2022-09-28 22:52:22 -0300

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' 
+This is a function that returns the dictionary description with simple
+data structure (list, dictionary, string, integer and boolean) for JSON
+serialization of an object
+'''
+
+
+def class_to_json(obj):
+    ''' just return the object's dictionary, which basically
+    contains all the attributes that describe the obj. '''
+    return obj.__dict__
---
+++ python-input_output/8-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" My class module
+"""
+class_to_json = __import__('8-class_to_json').class_to_json
+
+class MyClass:
+    """ My class
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
+
+m = MyClass("John")
+m.number = 89
+print(type(m))
+print(m)
+
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
