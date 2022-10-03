2e0729afed14d1e119233fc08853d91e9aa1d0e7
JoakG16 <4379@holbertonstudents.com>
2022-10-03 17:36:37 +0000

    task 1 class documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-
+    ''' class Rectangle constructor, using super class's method'''
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' Call the super class with id - this super call with use
         the logic of the __init__ of the Base class
