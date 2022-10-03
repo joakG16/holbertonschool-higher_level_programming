d22b98d5223d697fcb92bb2352584c67137b0ce1
JoakG16 <4379@holbertonstudents.com>
2022-10-03 16:46:59 +0000

    task 0 done

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' This module contains a user-defined class named "Base".
+This class will be the “base” of all other classes in this project. '''
+
+class Base:
+    ''' private class attribute '''
+    __nb_objects = 0
+    ''' class constructor '''
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
