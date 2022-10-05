989a218dd9336d5c1e1b0acce65ff8a243a718a1
JoakG16 <4379@holbertonstudents.com>
2022-10-05 18:50:43 +0000

    task 15 done

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 This class will be the “base” of all other classes in this project. '''
 
 
+import json
+
+
 class Base:
     ''' private class attribute '''
     __nb_objects = 0
@@ -13,3 +16,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' returns the JSON string representation of list_dictionaries '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            ''' json.dumps() function convert the raw data into JSON
+    format but stores it as a string '''
+            return json.dumps(list_dictionaries)
