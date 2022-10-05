d72c5280c1643696d3f49409f831afc8ae766afb
joakG16 <4379@holbertonschool.com>
2022-10-04 22:35:34 -0300

    task 10 done

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,3 +4,13 @@ In this module it's defined a new class "Square" that inherits from Rectangle
 '''
 from models.rectangle import Rectangle
 
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        ''' This super call will use the logic of the __init__ of the Rectangle
+        class. The width and height must be assigned to the value of size '''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        ''' string rep. of a square '''
+        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
