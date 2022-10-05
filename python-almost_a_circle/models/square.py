9a2c342de9c09ebed8a0b69c7118fceb664ae986
joakG16 <4379@holbertonschool.com>
2022-10-04 22:37:36 -0300

    missing class documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,6 +6,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    ''' A Square is a special Rectangle, so it makes sense this class Square
+    inherits from Rectangle. Now you have a Square class who has the same
+    attributes and same methods.'''
     def __init__(self, size, x=0, y=0, id=None):
         ''' This super call will use the logic of the __init__ of the Rectangle
         class. The width and height must be assigned to the value of size '''
