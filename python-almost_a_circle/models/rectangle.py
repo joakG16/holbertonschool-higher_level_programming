914e00c7f24a7f6be2735184635471163b0dbee5
JoakG16 <4379@holbertonstudents.com>
2022-10-05 16:33:46 +0000

    wrong identation fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -133,5 +133,5 @@ class Rectangle(Base):
     def to_dictionary(self):
         ''' this public method returns returns the dictionary
         representation of a Rectangle '''
-        return {'x': self.x, 'y': self.y
-, 'id': self.id, 'height': self.height, 'width': self.width}
+        return {'x': self.x, 'y': self.y, 'id':
+                self.id, 'height': self.height, 'width': self.width}
