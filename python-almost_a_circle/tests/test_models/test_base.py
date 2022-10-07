c6c6c1acaf0b290cf147f9bea3e11aeadf1f5223
joakG16 <4379@holbertonschool.com>
2022-10-06 22:24:53 -0300

    test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,21 +4,17 @@
 import unittest
 from models.base import Base
 
-class Test_Base(unittest.TestCase):
+class TestClass(unittest.TestCase):
     """Base class tests"""
     def test_validate(self):
         """ validate the id number, without args """
         A1 = Base()
-        self.assertEqual(A1.id, 1)
-        A2 = Base()
-        self.assertEqual(A2.id, 2)
-        A3 = Base()
-        self.assertEqual(A3.id, 3)
+        self.assertEqual(A1.id, 1) 
 
     def test_validate1(self):
         """ validate the id number, with args """
-        B1 = Base(25)
-        self.assertEqual(B1.id, 25)
+        B1 = Base(89)
+        self.assertEqual(B1.id, 89)
         B2 = Base(-13)
         self.assertEqual(B2.id, -13)
 
@@ -28,24 +24,40 @@ class Test_Base(unittest.TestCase):
             C1 = Base(3, 7)
 
     def test_validate3(self):
-        """ validate if the args is str """
-        D1 = Base("13")
-        self.assertEqual(D1.id, "13")
+        ''' should assign correct id '''
+        D1 = Base()
+        D2 = Base()
+        self.assertEqual(D2.id, 3)
 
     def test_validate4(self):
-        """ validate tuple """
-        E1 = Base((1, 2))
-        self.assertEqual(E1.id, (1, 2))
+        ''' should return an empty list '''
+        E1 = Base.to_json_string(None)
+        self.assertEqual(E1, "[]")
 
     def test_validate5(self):
-        """ validate dictionary """
-        F1 = Base({"hi": 10, "betty": 20})
-        self.assertEqual(F1.id, {"hi": 10, "betty": 20})
+        ''' should return an empty list '''
+        F1 = Base.to_json_string([])
+        self.assertEqual(F1, "[]")
 
     def test_validate6(self):
-        """ validate list """
-        G1 = Base([1, 2])
-        self.assertEqual(G1.id, [1, 2])
+        ''' should return a JSON string '''
+        G1 = Base.to_json_string([ { 'id': 12 }])
+        self.assertEqual(G1, '[{"id": 12}]')
+
+    def test_validate7(self):
+        ''' should return an empty list '''
+        H1 = Base.from_json_string(None)
+        self.assertEqual(H1, [])
+
+    def test_validate8(self):
+        ''' should return an empty list '''
+        I1 = Base.from_json_string("[]")
+        self.assertEqual(I1, [])
+
+    def test_validate9(self):
+        ''' should return a list '''
+        J1 = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(J1, [{'id': 89}])
 
 if __name__ == "__main__":
     unittest.main()
