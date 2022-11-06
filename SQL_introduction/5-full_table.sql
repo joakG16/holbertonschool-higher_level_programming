19871d845d9f2f19653429f72aa4a681c5bbf1a4
joakG16 <4379@holbertonschool.com>
2022-11-06 19:07:11 -0300

    task 5 done

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- a script that prints the full description of the table "first table" from database "hbtn_0c_0" in my MySQL server
+-- The query SHOW CREATE TABLE shows/displays the statement used to create the specified table. This displays the create statements along with the clauses.
+SHOW CREATE TABLE first_table;
