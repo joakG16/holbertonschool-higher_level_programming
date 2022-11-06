2c8a76c9d75388976e6a156a768f30ec64bbcdad
joakG16 <4379@holbertonschool.com>
2022-11-06 18:19:46 -0300

    some syntax error solucionated

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- a script thta deletes a database in my MySQL server
 -- If the database hbtn_0c_0 already exists, the script should throw an error message
-DELETE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
