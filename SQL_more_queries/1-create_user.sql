855228064a3ce19eb2f9b7b97a910aa1a1e8fb40
joakG16 <4379@holbertonschool.com>
2022-11-07 20:45:00 -0300

    unnessesary commands

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,5 +2,4 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- IF the user user_0d_1 already EXISTS, your script should NOT fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
