8d59c600db545f6f29d33bbd014529893e22723c
joakG16 <4379@holbertonschool.com>
2022-11-07 20:40:33 -0300

    refresh new user's privileges missing

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,3 +3,4 @@
 -- IF the user user_0d_1 already EXISTS, your script should NOT fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES
