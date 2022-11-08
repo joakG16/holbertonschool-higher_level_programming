f14b93c81612ee0d921277c4ef0724bf219efdc8
joakG16 <4379@holbertonschool.com>
2022-11-08 00:59:19 -0300

    task 14 done

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,8 +6,8 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
 
--- it will sum the times the established genre's id was used to identifiy each tv show's genre and 
--- then group it in a single record representing the genre 
+-- it will count the times the genre's name was repeated after matching its id with the other table
+-- (which only exposes how many times a show identified with certain genre), and group it in one single record each
 
 SELECT tv_genres.name AS genre, COUNT(*) AS 'number_of_shows'
 FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
