d2f4bc3015207cb3037eacfacd40b9b9e7c7ebae
joakG16 <4379@holbertonschool.com>
2022-11-08 01:06:05 -0300

    task 14 wrong output

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,7 +5,9 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
 
-SELECT name
-FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE show_id = '8'
-ORDER BY name ASC;
+SELECT *
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
