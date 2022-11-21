7dd76a65ebf7ca0985351b75115702d158a175c6
joakG16 <4379@holbertonschool.com>
2022-11-21 15:51:06 -0300

    extra columns deleted

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM cities INNER JOIN states ON cities.state_id\
-        = states.id ORDER BY cities.id ASC;")
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;")
     query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
