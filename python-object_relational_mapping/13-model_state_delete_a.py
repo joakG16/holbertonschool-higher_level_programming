c96111d4231ebdb109415749fd0d7688a46f24db
joakG16 <4379@holbertonschool.com>
2022-11-25 01:15:13 -0300

    task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""
-Write a script that changes the name of a State
-object from the database hbtn_0e_6_usa
+""" Write a script that deletes all State objects with a name
+containing the letter 'a' from the database hbtn_0e_6_usa
 """
 
 if __name__ == "__main__":
+    from sys import argv
     from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-    from sys import argv
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     session = Session(engine)
 
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
+    for state in session.query(State).order_by(State.id)\
+            .filter(State.name.contains('a')):
+        session.delete(state)
 
     session.commit()
     session.close()
