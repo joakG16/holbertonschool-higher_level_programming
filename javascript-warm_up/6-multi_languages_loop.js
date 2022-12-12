69f299b07a5bc7d77bd3b2fda4302b8acb532a0b
joakG16 <4379@holbertonschool.com>
2022-12-12 16:37:42 -0300

    some changes

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
+/* Array.lenght => Gets or sets the length of the array.
+This is a number one higher than the highest index in the array.
+*/
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < array.length; i++) {
+  console.log(array.length);
   console.log(array[i]);
 }
