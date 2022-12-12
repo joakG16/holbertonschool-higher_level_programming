6e4c8986da3b33ab5bf155af4ab318ff76a5c0fd
joakG16 <4379@holbertonschool.com>
2022-12-11 23:52:54 -0300

    task 8 and 9

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const symb = 'X';
+const i = process.argv[2];
+
+if (i && parseInt(i)) {
+  for (let j = 0; j < i; j++) {
+    console.log(symb.repeat(i));
+  }
+} else {
+  console.log('Missing size');
+}
