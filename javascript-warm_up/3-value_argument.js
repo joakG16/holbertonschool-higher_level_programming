f872491a2d2e59b548be6130459eeb74bc0ac776
joakG16 <4379@holbertonschool.com>
2022-12-12 16:21:01 -0300

    some changes

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
+if (process.argv[2]) { // This is an array containing the cmd. line arguments
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
