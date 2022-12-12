08a54f8574a339b4e90a3fb8d0115d9341d6aec7
joakG16 <4379@holbertonschool.com>
2022-12-11 23:48:11 -0300

    task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+
+const str = 'C is fun';
+const i = process.argv[2];
+
+if (!parseInt(i)) {
+  console.log('Missing number of occurrences');
+}
+for (let j = 0; j < i; j++) {
+  console.log(str);
+}
