8df0825f0b97cbb409cb8a588b5f97b9e540fcf7
joakG16 <4379@holbertonschool.com>
2022-12-12 16:33:30 -0300

    some changes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
-require('process');
+/* To keep in mind, process.argv[0] is the #!/usr/bin/node,
+but it is kept "hidden" so what you count in the command line
+starts from 1 (the executable) and so on.
+*/
 
 if (process.argv[3]) {
   console.log('Arguments found');
