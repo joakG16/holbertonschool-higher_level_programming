ca090c6e2db945a2e842b9bf71f6a3b63a406cbf
joakG16 <4379@holbertonschool.com>
2022-12-12 16:41:47 -0300

    some changes

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const i = process.argv[2];
 
 if (i && parseInt(i)) {
   for (let j = 0; j < i; j++) {
-    console.log(symb.repeat(i));
+    console.log(symb.repeat(i)); // repeats 'i' times the string 'X' in the line
   }
 } else {
   console.log('Missing size');
