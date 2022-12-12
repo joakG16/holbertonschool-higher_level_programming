4fb03e3ea531eabe35cc938214027c148fcf75d4
joakG16 <4379@holbertonschool.com>
2022-12-11 23:34:13 -0300

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 require('process');
 
-if (process.argv.length < 2) { // argu. starts from 1, not 0
+if (process.argv.length === 1) { // argu. starts from 1, not 0
     console.log('No argument');
 } else if (process.argv.length === 2) {
     console.log('Argument found');
