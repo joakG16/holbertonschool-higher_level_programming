1285bc663fdaa8be091fc5ea14a2bee5f97f929f
joakG16 <4379@holbertonschool.com>
2022-12-12 17:24:50 -0300

    compare function to solve sort

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,17 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 function secondBiggest (array) {
   if (array.length < 2) {
     return 0;
   }
-  const sortedArr = array.sort(); // in ascending order
+  /* array.map() => Calls a defined callback function on each element of an array,
+  and returns an array that contains the results.
+  */
+  const parsedArr = array.map(Number); // parsing array of str to numbers
+  const sortedArr = parsedArr.sort(function (a, b) {
+    return a - b;
+  }); // in ascending order
+  console.log(sortedArr);
   return sortedArr[array.length - 2];
 }
 console.log(secondBiggest(process.argv.slice(2))); // skip first two arguments
