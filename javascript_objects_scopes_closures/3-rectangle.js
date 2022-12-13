e282f0a27a7809675d92056bacebcbcb1ac40c7e
joakG16 <4379@holbertonschool.com>
2022-12-13 00:32:22 -0300

    task 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const symb = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(symb.repeat(this.width));
+    }
+  }
+};
