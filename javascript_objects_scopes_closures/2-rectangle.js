6510dec24bbab63c4b7f8c5833fea9e3bcee5ee4
joakG16 <4379@holbertonschool.com>
2022-12-13 00:18:51 -0300

    remade

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,8 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
-    }
-    if (h > 0) {
       this.height = h;
     }
   }
