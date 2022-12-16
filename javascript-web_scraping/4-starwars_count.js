b543256a825e77727f2a1e926f4af47d377e5b5b
Joaquin Dominguez <104533476+joakG16@users.noreply.github.com>
2022-12-16 17:12:09 +0000

    local server does not matches like the url would do, just check last part

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,9 +12,9 @@ request(apiURL, (err, res, body) => {
     const films = parseddata.results;
     let count = 0;
     for (const film of films) { // entering each film object in the films array
-      for (const character of film.characters) { // entering current film's array of characters
-        if (character === 'https://swapi-api.hbtn.io/api/people/18/') { // if the current URL of the charcter mactches
-        // for testing, use the updated character API URL: https://swapi-api.alx-tools.com/api/people/18/
+      for (const character of film.characters) { // entering current film's array of string of present-character's URLs
+        if (character.endsWith('/18/')) { // if the current URL of the charcter in the current film mactches
+          // for testing, use the updated character API URL: https://swapi-api.alx-tools.com/api/people/18/
           count += 1;
         }
       }
