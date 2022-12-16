f0e476ff7c2bb176a57aee2ebf148333a2344880
Joaquin Dominguez <104533476+joakG16@users.noreply.github.com>
2022-12-16 21:18:18 +0000

    task 6 remade

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -9,22 +9,27 @@ request(todoAPIurl, (err, res) => {
     console.error(err);
   } else {
     const parsedTasks = JSON.parse(res.body);
-
-    let userCounter = 1;
-    let completedTasks = 0;
     const userAndTask = {};
 
-    for (const taskObj of parsedTasks) {
-      if (taskObj.userId === userCounter) { // if the current object's user id property equals the defined counter
-        if (taskObj.completed === true) { // inside the same userid previously checked, check the completed property
-          completedTasks += 1;
+    for (const obj in parsedTasks) {
+      if (parsedTasks[obj].completed === true) {
+        if (!userAndTask[parsedTasks[obj].userId]) {
+          userAndTask[parsedTasks[obj].userId] = 1; // assign value 1 if key does not yet exist in the object
+        } else {
+          userAndTask[parsedTasks[obj].userId] += 1; // increment the value by 1 if it exists
         }
-      } else {
-        userAndTask[userCounter] = completedTasks; // store values counted from the current userId
-        userCounter += 1; // increment the defined counter to continue to the next user
-        completedTasks = 0; // reset the completed task for the next user
       }
     }
     console.log(userAndTask);
   }
 });
+
+//  if (taskObj.userId === userCounter) { // if the current object's user id property equals the defined counter
+//    if (taskObj.completed === true) { // inside the same userid previously checked, check the completed property
+//      completedTasks += 1;
+//    }
+//  } else {
+//    userAndTask[userCounter] = completedTasks; // store values counted from the current userId
+//    userCounter += 1; // increment the defined counter to continue to the next user
+//    completedTasks = 0; // reset the completed task for the next
+//  }
